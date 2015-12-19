261b2604a5f6da1875cd256a2d19a69c8b470457
Sam Royston <sfoxroyston@gmail.com>
2015-12-19 13:14:56 -0500

    improving telnet

--- esp-open-rtos/include/kern/telnet/telnet.c
+++ esp-open-rtos/include/kern/telnet/telnet.c
@@ -9,37 +9,39 @@ void telnetconn_callback(struct netconn * conn, enum netconn_evt evt, u16_t len)
 
 
 void telnet_init_callback(){
-	struct netconn *conn = netconn_new_with_callback(NETCONN_TCP, telnetconn_callback);
-	netconn_bind(conn, IP_ADDR_ANY, 80);
-	netconn_listen(conn);
+    struct netconn *conn = netconn_new_with_callback(NETCONN_TCP, telnetconn_callback);
+    netconn_bind(conn, IP_ADDR_ANY, 80);
+    netconn_listen(conn);
 }
 
 int process_conn(struct netconn *conn){
-	int i = 0;
-	return i;
+    int i = 0;
+    return i;
 }
 
 void run_server(){
-	printf("starting server...\n");
-	struct ip_info pTempIp;
- 	sdk_wifi_get_ip_info(0x00, &pTempIp);
-	printf("--IP:\"%d.%d.%d.%d\"\r\n", IP2STR(&pTempIp.ip));
-	static struct netconn *conn; \
-	conn = netconn_new(NETCONN_TCP);
+    printf("starting server...\n");
+    struct ip_info pTempIp;
+    sdk_wifi_get_ip_info(0x00, &pTempIp);
+    printf("--IP:\"%d.%d.%d.%d\"\r\n", IP2STR(&pTempIp.ip));
+    static struct netconn *conn; \
+    conn = netconn_new(NETCONN_TCP);
     struct netconn *new_conn;
 
-	netconn_bind(conn, IP_ADDR_ANY, 80);
-	netconn_listen(conn);
+    netconn_bind(conn, IP_ADDR_ANY, 80);
+    netconn_listen(conn);
 
-	while ( true )
-	{
-	   uint8_t err = netconn_accept(conn, &new_conn);
-	   if ( err != ERR_OK )
-	      continue;
-	   
-	   printf("message recvd \n");
-	   netconn_delete(new_conn);
-	}
+    while ( true )
+    {
+       uint8_t err = netconn_accept(conn, &new_conn);
+       if ( err != ERR_OK )
+          continue;
+       char *resp;
+       resp = "hollaback";
+       err = netconn_write(new_conn, resp, sizeof(*resp), 0);
+       printf("message recvd \n");
+       netconn_delete(new_conn);
+    }
 }
 
 void print_ip(uint32_t ip)
@@ -48,13 +50,13 @@ void print_ip(uint32_t ip)
     bytes[0] = ip & 0xFF;
     bytes[1] = (ip >> 8) & 0xFF;
     bytes[2] = (ip >> 16) & 0xFF;
-    bytes[3] = (ip >> 24) & 0xFF;	
+    bytes[3] = (ip >> 24) & 0xFF;   
     printf("IP: %d.%d.%d.%d\n", bytes[3], bytes[2], bytes[1], bytes[0]);        
 }
 
 void telnet_init(void){
 
-  	uart_set_baud(0, 115200);
+    uart_set_baud(0, 115200);
     printf("SDK version:%s\n", sdk_system_get_sdk_version());
 
     struct sdk_station_config config = {
@@ -72,11 +74,11 @@ void telnet_init(void){
     int status = 0; 
     uint32_t inc = 1;
     while(status == 0){
-    	if(inc % 10000000 == 0){
-    		printf("status: %d\n", status);
-	    	status = sdk_wifi_station_get_connect_status(); 
-	    }
-    	inc++;
+        if(inc % 10000000 == 0){
+            printf("status: %d\n", status);
+            status = sdk_wifi_station_get_connect_status(); 
+        }
+        inc++;
     }
     printf("connected!\n");
 
@@ -84,14 +86,14 @@ void telnet_init(void){
     struct ip_info info;
 
     if(sdk_wifi_get_ip_info(STATION_IF, &info)){
-    	printf("has valid addr...\n");
-    	print_ip(info.ip.addr);
+        printf("has valid addr...\n");
+        print_ip(info.ip.addr);
 
-	    xTaskCreate(&run_server, (signed char *)"runserver_task", 256, NULL, 2, NULL);
+        xTaskCreate(&run_server, (signed char *)"runserver_task", 256, NULL, 2, NULL);
     }
     else{
-    	printf("not so sure...\n");
-    	print_ip(info.ip.addr);
-    	printf("error getting ip addr, task not started\n");
+        printf("not so sure...\n");
+        print_ip(info.ip.addr);
+        printf("error getting ip addr, task not started\n");
     }
 }
